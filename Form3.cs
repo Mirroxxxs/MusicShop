3d7ca7b460299899a1a93b89952fb6a4e132d75a
GlazovEgor <147654835+Mirroxxxs@users.noreply.github.com>
2024-05-21 13:41:43 +0300

    Изменение комментариев и переменных.cs

--- Form3.cs
+++ Form3.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+using System;
 using System.Data.SqlClient;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace MusicShop
@@ -17,65 +10,88 @@ namespace MusicShop
         {
             InitializeComponent();
         }
+
+        // Строка подключения к базе данных
         private string connection = "Data Source=EGORKA\\SQLEXPRESS;Initial Catalog=MusicShop;Integrated Security=True;";
+
+        // Класс для работы с базой данных
         public class Manager
         {
             private string connection;
+
             public Manager(string connection)
             {
                 this.connection = connection;
             }
+
+            // Метод для инициализации пользователя
             public bool Inicilizate(string login, string password)
             {
                 using (SqlConnection connect = new SqlConnection(connection))
                 {
-                    connect.Open(); string query = "SELECT COUNT(*) FROM identification WHERE login = @Login AND password = @Password";
-                    SqlCommand command = new SqlCommand(query, connect); command.Parameters.AddWithValue("@Login", login);
+                    connect.Open();
+                    string query = "SELECT COUNT(*) FROM identification WHERE login = @Login AND password = @Password";
+                    SqlCommand command = new SqlCommand(query, connect);
+                    command.Parameters.AddWithValue("@Login", login);
                     command.Parameters.AddWithValue("@Password", password);
                     int count = (int)command.ExecuteScalar();
                     return count > 0;
                 }
             }
 
+            // Метод для регистрации пользователя
             public void RegisterUser(string login, string password)
             {
                 using (SqlConnection connect = new SqlConnection(connection))
                 {
-                    connect.Open(); string query = "INSERT INTO identification (login, password) VALUES (@Login, @Password)";
-                    SqlCommand command = new SqlCommand(query, connect); command.Parameters.AddWithValue("@Login", login);
+                    connect.Open();
+                    string query = "INSERT INTO identification (login, password) VALUES (@Login, @Password)";
+                    SqlCommand command = new SqlCommand(query, connect);
+                    command.Parameters.AddWithValue("@Login", login);
                     command.Parameters.AddWithValue("@Password", password);
                     command.ExecuteNonQuery();
                 }
             }
         }
-        private void button2_Click(object sender, EventArgs e)
+
+        // Обработчик события нажатия кнопки "Назад"
+        private void buttonExit_Click(object sender, EventArgs e)
         {
+            // Создание экземпляра главной формы и ее отображение
             Form1 form1 = new Form1();
             form1.Show();
+
+            // Скрытие текущей формы
             Hide();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        // Обработчик события нажатия кнопки "Войти"
+        private void buttonLogin_Click(object sender, EventArgs e)
         {
-            string login = textBoxLogin.Text; string password = textBoxPassword.Text;
+            string login = textBoxLogin.Text;
+            string password = textBoxPassword.Text;
             Manager manager = new Manager(connection);
+
             if (manager.Inicilizate(login, password))
             {
                 MessageBox.Show("Вы успешно вошли в систему!");
                 Form4 form4 = new Form4();
                 form4.Show();
+
+                // Скрытие текущей формы
                 Hide();
             }
             else
             {
                 MessageBox.Show("Неправильное имя пользователя или пароль!");
             }
-        
         }
+
+        // Переменная для отображения пароля
         private bool isPasswordVisible = false;
 
-   
-        private void button3_Click(object sender, EventArgs e)
+        // Обработчик события нажатия кнопки "Показать пароль"
+        private void buttonPasswordVisible_Click(object sender, EventArgs e)
         {
             if (isPasswordVisible)
             {
@@ -84,7 +100,7 @@ namespace MusicShop
             }
             else
             {
-                // Показать текст
+                // Показать текст пароля
                 textBoxPassword.PasswordChar = '\0';
                 isPasswordVisible = true;
             }
