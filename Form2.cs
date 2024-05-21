8ffc3c34806521e433c9c6a674c26525c15cac41
GlazovEgor <147654835+Mirroxxxs@users.noreply.github.com>
2024-05-21 13:36:12 +0300

    Изменение комментариев и переменных.cs

--- Form2.cs
+++ Form2.cs
@@ -1,6 +1,7 @@
-﻿using System;
+using System;
 using System.Data.SqlClient;
 using System.Windows.Forms;
+
 namespace MusicShop
 {
     public partial class Form2 : Form
@@ -9,43 +10,62 @@ namespace MusicShop
         {
             InitializeComponent();
         }
+
+        // Строка подключения к базе данных
         private string connection = "Data Source=EGORKA\\SQLEXPRESS;Initial Catalog=MusicShop;Integrated Security=True;";
+
+        // Обработчик события нажатия кнопки "Назад"
         private void button2_Click(object sender, EventArgs e)
         {
+            // Создание экземпляра главной формы и ее отображение
             Form1 form1 = new Form1();
             form1.Show();
+
+            // Скрытие текущей формы
             Hide();
         }
+
+        // Класс для работы с базой данных
         public class Manager
         {
             private string connection;
+
             public Manager(string connection)
             {
                 this.connection = connection;
             }
+
+            // Метод для инициализации пользователя
             public bool Inicilizate(string login, string password)
             {
                 using (SqlConnection connect = new SqlConnection(connection))
                 {
-                    connect.Open(); string query = "SELECT COUNT(*) FROM identification WHERE login = @Login AND password = @Password";
-                    SqlCommand command = new SqlCommand(query, connect); command.Parameters.AddWithValue("@Login", login);
+                    connect.Open();
+                    string query = "SELECT COUNT(*) FROM identification WHERE login = @Login AND password = @Password";
+                    SqlCommand command = new SqlCommand(query, connect);
+                    command.Parameters.AddWithValue("@Login", login);
                     command.Parameters.AddWithValue("@Password", password);
                     int count = (int)command.ExecuteScalar();
                     return count > 0;
                 }
             }
 
+            // Метод для регистрации пользователя
             public void RegisterUser(string login, string password)
             {
                 using (SqlConnection connect = new SqlConnection(connection))
                 {
-                    connect.Open(); string query = "INSERT INTO identification (login, password) VALUES (@Login, @Password)";
-                    SqlCommand command = new SqlCommand(query, connect); command.Parameters.AddWithValue("@Login", login);
+                    connect.Open();
+                    string query = "INSERT INTO identification (login, password) VALUES (@Login, @Password)";
+                    SqlCommand command = new SqlCommand(query, connect);
+                    command.Parameters.AddWithValue("@Login", login);
                     command.Parameters.AddWithValue("@Password", password);
                     command.ExecuteNonQuery();
                 }
             }
         }
+
+        // Обработчик события нажатия кнопки "Зарегистрироваться"
         private void registrate_Click(object sender, EventArgs e)
         {
             if (textBoxLogin.Text != "" && textBoxPassword.Text != "")
@@ -57,12 +77,17 @@ namespace MusicShop
                 MessageBox.Show(this, "Вы успешно зарегистрированы", "", MessageBoxButtons.OK);
                 Form4 form4 = new Form4();
                 form4.Show();
+
+                // Скрытие текущей формы
                 Hide();
             }
         }
+
+        // Переменная для отображения пароля
         private bool isPasswordVisible = false;
 
-        private void button1_Click(object sender, EventArgs e)
+        // Обработчик события нажатия кнопки "Показать пароль"
+        private void buttonPasswordVisible_Click(object sender, EventArgs e)
         {
             if (isPasswordVisible)
             {
@@ -71,7 +96,7 @@ namespace MusicShop
             }
             else
             {
-                // Показать текст
+                // Показать текст пароля
                 textBoxPassword.PasswordChar = '\0';
                 isPasswordVisible = true;
             }
